07ed799d8e5f3595a7b56a5fa9037f09153ee87f
Omar Nasser <mashjari96@gmail.com>
2017-01-19 03:02:16 +0400

    Added Bourbon and Neat

--- _js/app.js
+++ _js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	function smoothScroll (duration) {
+	/*function smoothScroll (duration) {
 		$('a[href^="#"]').on('click', function(event) {
 
 			var target = $( $(this).attr('href') );
@@ -14,5 +14,40 @@ $(document).ready(function(){
 		});
 	}
 
-	smoothScroll(1000);
+	smoothScroll(1000);*/
+
+	(function (jQuery) {
+		jQuery.mark = {
+			jump: function (options) {
+				var defaults = {
+					selector: 'a.scroll-on-page-link'
+				};
+				if (typeof options == 'string') {
+					defaults.selector = options;
+				}
+
+				options = jQuery.extend(defaults, options);
+				return jQuery(options.selector).click(function (e) {
+					var jumpobj = jQuery(this);
+					var target = jumpobj.attr('href');
+					var thespeed = 1000;
+					var offset = jQuery(target).offset().top;
+					jQuery('html,body').animate({
+						scrollTop: offset
+					}, thespeed, 'swing');
+					e.preventDefault();
+				});
+			}
+		};
+	})(jQuery);
+
+
+	jQuery(function(){
+		jQuery.mark.jump();
+	});
+
+
+
 });
+
+
