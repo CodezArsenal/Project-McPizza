3fd05994a03ea236be4aabd51412e219f8c6d948
Omar Nasser <mashjari96@gmail.com>
2017-02-02 00:18:57 +0400

    menuV2 Loaded
    using handlebars + ajax

---
+++ _js/handlebarsTasks.js
@@ -0,0 +1,22 @@
+/**
+ * Created by omar on 1/31/17.
+ */
+var source = $('#handlebarsTemp').html();
+var template = Handlebars.compile(source);
+//var menu = JSON.parse(document.getElementById('menuJSON').textContent);
+var menu = Object;
+
+$.getJSON('_assets/content/menuV2.json',function(data){
+	//console.log('success');
+	//console.log(data);
+	menu = data;
+
+	var html = template(menu);
+	$('.orderOnline').html(html);
+});
+
+
+/*$.getJSON('../menu.json', function(json){
+	console.log(json);
+});*/
+
